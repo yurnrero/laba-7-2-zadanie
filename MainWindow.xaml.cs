1a16e933aaf433a39511801b3b7b4fd0b6e32107
Admin <Admin@DESKTOP-7MCGF0R>
2024-10-30 13:28:10 +0700

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace laba_7_2_zadanie
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Random _random = new Random();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RunawayButton_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            double newLeft = _random.NextDouble() * (this.Width - RunawayButton.Width);
+            double newTop = _random.NextDouble() * (this.Height - RunawayButton.Height);
+
+            DoubleAnimation leftAnimation = new DoubleAnimation(newLeft, TimeSpan.FromMilliseconds(300));
+            DoubleAnimation topAnimation = new DoubleAnimation(newTop, TimeSpan.FromMilliseconds(300));
+
+            RunawayButton.BeginAnimation(Canvas.LeftProperty, leftAnimation);
+            RunawayButton.BeginAnimation(Canvas.TopProperty, topAnimation);
+        }
+    }
+}
+
+   
